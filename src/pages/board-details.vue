e2f681da411fa4f0ddea66e44d4fee4b676a3738
RZShachar <RZShachar@gmail.com>
2022-03-25 00:17:07 +0200

    Adam my dearest... you are the best, you are my dnd <3

--- src/pages/board-details.vue
+++ src/pages/board-details.vue
@@ -77,7 +77,7 @@ export default {
           const group = board.groups.find(group => group.id === groupId)
           group.tasks.splice(ev.addedIndex, 0, ev.payload)
           this.$store.dispatch({ type: 'saveBoard', boardToSave: board })
-        }, 10)
+        }, 1)
       }
 
     },
