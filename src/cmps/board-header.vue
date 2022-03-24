cd861f7555fc675debcbdf1a58ab5aee871994b5
Adam <adampn462@gmail.com>
2022-03-24 17:51:43 +0200

    After changing vars sheet

--- src/cmps/board-header.vue
+++ src/cmps/board-header.vue
@@ -1,6 +1,5 @@
 <template>
-    <!-- TODO remove inline style -->
-    <header :class="headerBgc" class="board-header flex space-between align-center">
+    <header class="board-header flex space-between align-center">
         <div class="board-nav flex">
             <button class="open-sidenav"> > </button>
             <button>Board</button>
@@ -13,6 +12,7 @@
             </div>
             <button class="invite-btn">+ Invite</button>
         </div>
+
         <div>
             <button>Filter</button>
             <button>Show menu</button>
@@ -28,11 +28,7 @@ export default {
     created() {
         console.log(this.board);
     },
-    computed: {
-        headerBgc() {
-            return {'trans-header': /board\//g.test(this.$route.fullPath)}
-        }
-    }
+
 }
 
 </script>
