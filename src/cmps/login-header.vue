4dad181e35b70218cc93bf31e5afbdf8cf8a9417
Adam <adampn462@gmail.com>
2022-03-26 18:10:13 +0300

    remove console logs

--- src/cmps/login-header.vue
+++ src/cmps/login-header.vue
@@ -1,5 +1,8 @@
 <template>
     <header class="main-header login-logout-header">
-        <div class="logo"><span>Twello</span></div>
+        <div class="logo flex align-center">
+            <span class="logo-img"></span>
+            <span class="logo-txt">Twello</span>
+        </div>
     </header>
 </template>
