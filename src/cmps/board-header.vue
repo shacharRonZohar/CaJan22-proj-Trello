38bea3019a562c1165a3b48af6c76e5f33423d3f
Adam <adampn462@gmail.com>
2022-03-28 12:25:16 +0300

    Added edit board name functionality

--- src/cmps/board-header.vue
+++ src/cmps/board-header.vue
@@ -3,9 +3,11 @@
         <div class="board-nav flex">
             <!-- <div class="open-sidenav flex"><span class="svg-icon arrow-right"></span></div> -->
 
-            <button class="board-btn btn"><span class="svg-icon board-icon"></span> Board</button>
+            <button class="board-btn btn">
+                <span class="svg-icon board-icon"></span> Board
+            </button>
 
-            <div class="board-name flex" contenteditable>{{board.title}}</div>
+            <div class="board-name flex" @blur="onSaveBoardName" contenteditable>{{ board.title }}</div>
 
             <button class="star-btn btn"></button>
 
@@ -14,15 +16,21 @@
                 <div>CB</div>
                 <div>SZ</div>
             </div>
-            
-            <button class="invite-btn btn"><span class="invite"></span> Invite</button>
+
+            <button class="invite-btn btn">
+                <span class="invite"></span> Invite
+            </button>
         </div>
 
         <div class="board-action">
             <button class="filter-btn btn">
-                <span><span class="svg-icon filter"></span> Filter</span>
+                <span>
+                    <span class="svg-icon filter"></span> Filter
+                </span>
             </button>
-            <button @click="onOpenMenu" class="right-nav-btn btn"><span class="dots"></span>Show menu</button>
+            <button @click="onOpenMenu" class="right-nav-btn btn">
+                <span class="dots"></span>Show menu
+            </button>
         </div>
     </header>
 </template>
@@ -40,6 +48,12 @@ export default {
     },
     created() {
         console.log(this.board);
+    },
+    methods: {
+        async onSaveBoardName(ev) {
+            this.board.title = ev.target.innerText
+            this.$store.dispatch({type: 'saveBoard', boardToSave: this.board})
+        },
     }
 
 }
