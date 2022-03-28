cee2a1310b1704436b01b9992359daf4afc8484b
RZShachar <RZShachar@gmail.com>
2022-03-28 22:40:23 +0300

    Login page design

--- src/pages/login-signup.vue
+++ src/pages/login-signup.vue
@@ -1,32 +1,33 @@
 <template>
     <section class="auth-actions-container">
-        <form class="auth-input-form" @submit.prevent.stop="onUserAction">
-            <input
-                v-model="user.username"
-                type="text"
-                class="username"
-                placeholder="username"
-                required
-            />
-            <input
-                v-model="user.password"
-                type="password"
-                class="password"
-                placeholder="password"
-                required
-            />
-            <input
-                v-if="currAction === 'signup'"
-                v-model="user.fullname"
-                type="text"
-                class="fullname"
-                placeholder="fullname"
-                required
-            />
-            <button>{{ currAction }}</button>
-        </form>
-        <button @click="onGoogleLogin" class="btn google-login">Login with google</button>
-        <button @click="onLogout" class="signout">Logout</button>
+        <div class="auth-form-container">
+            <h1 class="title">Log in to Trello</h1>
+            <form class="auth-input-form" @submit.prevent.stop="onUserAction">
+                <input
+                    v-model="user.username"
+                    type="text"
+                    class="username"
+                    placeholder="username"
+                    required
+                />
+                <input
+                    v-model="user.password"
+                    type="password"
+                    class="password"
+                    placeholder="password"
+                    required
+                />
+                <input
+                    v-if="currAction === 'signup'"
+                    v-model="user.fullname"
+                    type="text"
+                    class="fullname"
+                    placeholder="fullname"
+                    required
+                />
+                <button>{{ currAction }}</button>
+            </form>
+        </div>
     </section>
 </template>
 
