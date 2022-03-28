8d3af652a6b8ceecfd251c2af122e37e73df5ab6
Adam <adampn462@gmail.com>
2022-03-28 08:43:34 +0300

    Done basic styling on add board modal

--- src/cmps/add-board.vue
+++ src/cmps/add-board.vue
@@ -1,30 +1,35 @@
 <template>
     <section class="add-board">
-        <div class="header">
-            <span>Back </span>
-            <span>Create board</span>
-            <span> X</span>
-            <hr>
+        <div class="header flex">
+            <span class="back-btn" @click="onBack">B</span>
+            <span class="title">Create board</span>
+            <span class="exit-btn" @click="onClose">X</span>
         </div>
 
         <div class="body">
-            <span>Background</span>
-            <ul class="img-list clean-list flex">
-                <li v-for="img in imgs" :key="img.id">
-                    <img @click="setBackgroundImg(img)" :src="img.urls.thumb" alt />
-                </li>
-            </ul>
-            <ul class="color-list clean-list flex">
-                <li v-for="color in colors" :key="color">
-                    <div 
-                    class="color" 
-                    @click="setBackgroundColor(color)" 
-                    :style="{backgroundColor: color}">
-                    </div>
-                </li>
-            </ul>
-            <input v-model="boardToAdd.title" type="text" />
-            <button @click="onCreateBoard">Create</button>
+            <hr>
+            <div class="bg-panel">
+                <span class="background-title">Background</span>
+                <ul class="img-list clean-list flex">
+                    <li v-for="img in imgs" :key="img.id">
+                        <img class="img" @click="setBackgroundImg(img)" :src="img.urls.thumb" alt>
+                    </li>
+                </ul>
+                <ul class="color-list clean-list flex">
+                    <li v-for="color in colors" :key="color">
+                        <div 
+                        class="color" 
+                        @click="setBackgroundColor(color)" 
+                        :style="{backgroundColor: color}">
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="input-panel">
+                <span class="input-title">Board title <span class="asterisk">*</span></span>
+                <input v-model="boardToAdd.title" type="text" />
+            </div>
+            <button class="create-btn btn" @click="onCreateBoard">Create</button>
         </div>
     </section>
 </template>
@@ -35,7 +40,7 @@ import { imgService } from '../services/imgService.js'
 
 
 export default {
-    emits: ['addedBoard'],
+    emits: ['addedBoard', 'closeModal'],
     data() {
         return {
             boardToAdd: boardService.getEmptyBoard(),
@@ -49,22 +54,22 @@ export default {
     methods: {
         async displayImgs() {
             const res = await imgService.queryPhotos(this.searchTxt)
-            console.log(res.results.splice(1, 4));
             this.imgs = res.results.splice(1, 4)
         },
         setBackgroundImg(img) {
-            console.log(img);
             this.boardToAdd.style.imgUrl = img.urls.full
             this.boardToAdd.style.color = ''
         },
         setBackgroundColor(color) {
-            console.log(color);
             this.boardToAdd.style.color = color
             this.boardToAdd.style.imgUrl = ''
         },
         onCreateBoard() {
-            console.log('creating - ', this.boardToAdd);
+            if (this.boardToAdd.title.trim() === '') return
             this.$store.dispatch({type: 'saveBoard', boardToSave: this.boardToAdd})
+            this.$emit('addedBoard')
+        },
+        onClose() {
             this.$emit('addedBoard')
         }
     },
