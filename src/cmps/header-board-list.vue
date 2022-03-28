25a67a626172393681342faaccbbc823b3e7d4c1
Adam <adampn462@gmail.com>
2022-03-28 11:09:23 +0300

    done basic board list functionality

--- src/cmps/header-board-list.vue
+++ src/cmps/header-board-list.vue
@@ -1,17 +1,57 @@
 <template>
     <div class="header-board-list">
         <span>Boards</span>
-        <button>x</button>
-        <ul>
-            <li></li>
+        <button class="btn" @click="onClose">x</button>
+        <ul class="board-list clean-list">
+            <li @click="goToBoard(board)" v-for="board in boards" :key="board._id">
+                <div class="mini-pic" :style="{ backgroundImage: img(board) }"></div>
+                <span>{{ board.title }}</span>
+            </li>
         </ul>
     </div>
 </template>
 
 <script>
 export default {
-    // props: {
-    //     type: Array
-    // }
+    props: {
+        boards: Array
+    },
+    emits: ['closeModal'],
+    created() {
+        console.log(this.boards);
+    },
+    methods: {
+        goToBoard(board){
+            console.log(board);
+            this.$router.push(`/board/${board._id}`)
+        },
+        onClose() {
+            this.$emit('closeModal')
+        },
+        img(board) {
+            const url = new URL(`${board.style.imgUrl}`, import.meta.url).href
+            return `url(${url})`
+        },
+    },
+    computed: {
+
+    }
 }
 </script>
+
+<style>
+.header-board-list {
+    border: 1px solid black;
+    padding: 10px;
+    position: absolute;
+    z-index: 1;
+    top: 50px;
+    background-color: #fff;
+}
+.mini-pic {
+    width: 40px;
+    height: 40px;
+    display: inline-block;
+    background-size: cover;
+}
+</style>
