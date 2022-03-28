f95a18fd14594977de1cb09ce9dba80c1747694e
Adam <adampn462@gmail.com>
2022-03-28 13:18:07 +0300

    Added new board-filter-modal sheet to styles

---
+++ src/cmps/board-filter-modal.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="board-filter-modal">
+        <div class="header flex space-between">
+            <span class="title">filter</span>
+            <button class="close-btn btn">x</button>
+        </div>
+        <hr>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.board-filter-modal {
+    border: 1px solid black;
+    width: 304px;
+    background-color: #fff;
+    position: absolute;
+    right: 10px;
+    padding: 10px;
+}
+</style>
