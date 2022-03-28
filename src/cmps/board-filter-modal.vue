8194cac43d08d16c8991848e835203752e831698
Adam <adampn462@gmail.com>
2022-03-28 14:51:00 +0300

    Added basic filter modal

--- src/cmps/board-filter-modal.vue
+++ src/cmps/board-filter-modal.vue
@@ -1,24 +1,37 @@
 <template>
     <div class="board-filter-modal">
-        <div class="header flex space-between">
-            <span class="title">filter</span>
+        <div class="header flex space-between align-center">
+            <span class="title">Filter</span>
             <button class="close-btn btn">x</button>
         </div>
         <hr>
+
+        <div class="body">
+            <form>
+                <p class="input-title">Keyword</p>
+                <input 
+                class="filter-input" 
+                v-model="filterBy.txt" 
+                placeholder="Enter a Keyword..."
+                type="text">
+                <p class="input-desc">Search cards, members, labels, and more.</p>
+            </form>
+        </div>
     </div>
 </template>
 
 <script>
-
+export default {
+    data() {
+        return {
+            filterBy: {
+                txt: ''
+            }
+        }
+    }
+}
 </script>
 
 <style>
-.board-filter-modal {
-    border: 1px solid black;
-    width: 304px;
-    background-color: #fff;
-    position: absolute;
-    right: 10px;
-    padding: 10px;
-}
+
 </style>
