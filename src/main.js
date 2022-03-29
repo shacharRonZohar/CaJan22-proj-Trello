2662f1fd1b1435132b21cf8c4c6fcd9d6ad90239
RZShachar <RZShachar@gmail.com>
2022-03-29 11:34:15 +0300

    Added more design to auth-actions page

--- src/main.js
+++ src/main.js
@@ -3,14 +3,10 @@ import app from './app.vue'
 import router from './router'
 import store from './store'
 import { focusDirective, closeOnBlur } from './directives'
-<<<<<<< HEAD
 import gAuthPlugin from 'vue3-google-oauth2'
-=======
 import Datepicker from '@vuepic/vue-datepicker'
-import '@vuepic/vue-datepicker/dist/main.css';
->>>>>>> ce313caa20106e9b7d27344ee8ee319f88fda4d7
+import '@vuepic/vue-datepicker/dist/main.css'
 import './styles/styles.scss'
-
 const gAuthCLientID = '1085095174420-s26dkcaicaiubfjpebgqjpb3dopricv9.apps.googleusercontent.com'
 
 
