4553320b066f31b6931ced06232baf823809979e
RZShachar <RZShachar@gmail.com>
2022-03-30 16:24:42 +0300

    Added design for profile modal

--- src/cmps/profile-modal.vue
+++ src/cmps/profile-modal.vue
@@ -1,12 +1,29 @@
 <template>
-    <section @click="onLogout">Logout</section>
+    <section @click.stop class="profile-modal" @click="onLogout">
+        <div class="header">Account</div>
+        <div class="user">
+            <div class="profile-icon" :style="{ backgroundImage: `url(${user.imgUrl})` }"></div>
+            <div class="details">
+                <span class="fullname">{{ user.fullname }}</span>
+                <small class="username">{{ user.username }}</small>
+            </div>
+        </div>
+        <div class="profile-btns">
+            <span class="profile-btn">Profile</span>
+            <span class="activity-btn">Activity</span>
+            <span class="Cards-btn">Cards</span>
+        </div>
+        <div @click="onLogout" class="logout">Logout</div>
+    </section>
 </template>
 
 <script>
 // import appHeader from './components/app-header.vue'
 import { authService } from '../services/auth.service.js'
 export default {
-    // props: [''],
+    props: {
+        user: Object
+    },
     components: {},
     created() { },
     data() {
